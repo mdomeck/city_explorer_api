2e531038b5bba4d545c3e0d11d2c2548c77ca2fe
Meghan Domeck <mdomeck@aol.com>
2020-07-14 17:26:13 -0400

    testing location

--- server.js
+++ server.js
@@ -2,50 +2,46 @@
 
 // libraries
 const express = require('express');
-
-const app = express(); //server library
-
-require('dotenv').config(); //privacy library
-
 const cors = require('cors'); //really bad body guard lets anyone talk to server
-
 const superagent = require('superagent'); //gets stuff from API
 const { json } = require('express');
+require('dotenv').config(); //privacy library
 
-
+const app = express(); //server library
 app.use(cors()); // allow all clients into our server
 
 
 // global variables
 const PORT = process.env.PORT || 3001;
-let weatherArr = [];
 
 //=============LOCATION========================//
-app.get('/location', (request, response) => {
+app.get('/location', handleLocation);
+
+function handleLocation(request, response){
   
   let city = request.query.city;
-  let geoData = require('./data/location.json');
-  let url = `https://us1.locationiq.com/v1/search.php`
-
+  // let geoData = require('./data/location.json');
+  let url = `https://us1.locationiq.com/v1/search.php`;
+  
   let queryParams = {
     key: process.env.GEOCODE_API_KEY,
     q: city,
     format: 'json',
     limit: 1
   }
-
+  
   superagent.get(url)
   .query(queryParams)
   .then(resultsFromSuperagent => {
     console.log('these are my results from superagent:', resultsFromSuperagent.body);
-  let geoData = resultsFromSuperagent.body;
-  const obj = new Location(city, geoData);
-  response.status(200).send(obj);
+    let geoData = resultsFromSuperagent.body;
+    const obj = new Location(city, geoData);
+    response.status(200).send(obj);
   }).catch((error) => { 
     console.log('ERROR', error);
-    response.status(500).send('Sorry, somehing went wrong');
-    })
-}
+    response.status(500).send('Sorry, something went wrong');
+  });
+};
 
 function Location(location, geoData) {
   this.search_query = location;
@@ -58,10 +54,10 @@ function Location(location, geoData) {
 app.get('/weather', (request, response) => {
   let weatherInfo = require('./data/weather.json')
   
-
+  
   //use a .map not a forEach
-  weatherInfo['data'].forEach(date => {
-    weatherArr.push(new Weather(date));
+  let weatherArr = weatherInfo['data'].map(date => {
+    return new Weather(date);
   })
   response.send(weatherArr);
   
